0fb78061b00ee8906bb1ed2ead84eb210268c208
mingrammer <k239507@gmail.com>
2017-07-31 00:37:51 +0900

    Add loss functions

--- common/functions.py
+++ common/functions.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+###
+# Activation functions
+###
 def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
@@ -24,4 +27,14 @@ def softmax(a):
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
-    
+
+###
+# Loss functions
+###
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
