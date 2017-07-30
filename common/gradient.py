3e8892fb633abb3341d512c571cbb8ba49a971b4
mingrammer <k239507@gmail.com>
2017-07-31 00:42:42 +0900

    Add gradient functions

---
+++ common/gradient.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp = x[idx]
+
+        x[idx] = tmp + h
+        fxh1 = f(x)
+
+        x[idx] = tmp - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp # restore the value
+    return grad
+
+
+def numerical_gradient(f, x):
+    if x.ndim == 1:
+        return _numerical_gradient_no_batch(f, x)
+    else:
+        grad = np.zeros_like(x)
+
+        for idx, x in enumerate(x):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val
+        it.iternext()
+    return grad
+
+
+def gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val
+        it.iternext()
+    return grad
