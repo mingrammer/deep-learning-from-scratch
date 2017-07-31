7bf0ba9a614f4983622f9ae460a9e9ae87281a3b
mingrammer <k239507@gmail.com>
2017-08-01 00:18:37 +0900

    Speed up the neural network training

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -5,7 +5,6 @@ sys.path.append(os.pardir)
 import numpy as np
 from common.functions import *
 from common.gradient import numerical_gradient
-from common.gradient import gradient
 
 
 class TwoLayerNet:
@@ -53,14 +52,26 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_w, self.params['b2'])
         return grads
 
-    # x: training input data
-    # t: training output label
     def gradient(self, x, t):
-        loss_w = lambda w: self.loss(x, t)
-        
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
         grads = {}
-        grads['W1'] = gradient(loss_w, self.params['W1'])
-        grads['b1'] = gradient(loss_w, self.params['b1'])
-        grads['W2'] = gradient(loss_w, self.params['W2'])
-        grads['b2'] = gradient(loss_w, self.params['b2'])
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
         return grads
