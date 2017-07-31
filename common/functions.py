d7575f24a6b0d2d7ed54d797e402e57759f1d186
mingrammer <k239507@gmail.com>
2017-08-01 00:16:25 +0900

    Improve the functions

--- common/functions.py
+++ common/functions.py
@@ -13,6 +13,10 @@ def sigmoid(x):
     return 1.0 / (1 + np.exp(-x))
 
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
 def relu(x):
     return np.maximum(0, x)
 
@@ -21,12 +25,14 @@ def identity_function(x):
     return x
 
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-    return y
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
 ###
 # Loss functions
@@ -36,5 +42,10 @@ def mean_squared_error(y, t):
 
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t]))
